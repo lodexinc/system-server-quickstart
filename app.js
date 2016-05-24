d0075e3d571b49954c280b79e4db86a3f677c290
Erwan Carriou <erwan.carriou@mac.com>
2016-05-24 21:21:11 +0200

    - add app.json

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
+// to get require method in global
+// to be able to use it in runtime
+global._require = require;
+
 // require runtime
 var runtime = require('system-runtime');
 
 // require admin system
 var admin = require('./system-server-admin.json');
 
-// to get require method in global
-// to be able to use it in runtime
-global._require = require;
+// require the app
+var app = require('./app.json');
 
-// admin system
+// run admin system
 var systemId = runtime.require('db').system(admin);
 runtime.require(systemId).main();
 
-// set log level
+// run the app the
+var appId = runtime.require('db').system(app);
+runtime.require(appId).main();
+
+// set log level for debug
 runtime.require('logger').level('debug');
-
-// create a system
-var system = runtime.system('example');
-
-// add your code in the main method of the system
-system.on('main', function main() {
-    console.log('Hello world !');
-});
-
-// run your system
-system.main();
