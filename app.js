7c542d9623cd7cdd9b6d061725dde1b8892e0d63
Erwan Carriou <erwan.carriou@mac.com>
2016-11-13 16:57:09 +0100

    Update readme

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 var runtime = require('system-runtime');
-var id = '';
 
-// install the admin
-id = runtime.install('system-admin-server');
-// start the admin
-runtime.start(id);
+// install and start the admin
+runtime.install('system-admin-server');
 
-// install the app
-id = runtime.install('app.json');
-// start the app
-runtime.start(id);
+// install and start the app
+runtime.install('app.json');
 
 // set log level for debug
 runtime.require('logger').level('debug');
